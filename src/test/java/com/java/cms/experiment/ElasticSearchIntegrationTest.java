9cf18c04acb7b01fdb57357ede3185daaadffd04
Niko Stahl <r.niko.stahl@gmail.com>
2016-08-11 22:14:30 -0400

    Clean up whitespace

--- src/test/java/com/java/cms/experiment/ElasticSearchIntegrationTest.java
+++ src/test/java/com/java/cms/experiment/ElasticSearchIntegrationTest.java
@@ -8,7 +8,6 @@ import org.apache.http.nio.entity.NStringEntity;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.junit.Test;
-
 import java.io.IOException;
 import java.util.Collections;
 
@@ -19,14 +18,14 @@ import static org.junit.Assert.*;
  */
 public class ElasticSearchIntegrationTest {
 
-    // documented here https://www.elastic.co/guide/en/elasticsearch/client/java-rest/current/index.html
+    // documented at www.elastic.co/guide/en/elasticsearch/client/java-rest/current/index.html
     private RestClient restClient;
+    // 14 day trial starting August 11th, 2016 - see cloud.elastic.co
     private String hostUrl = "1a899325e2c31027087ee0973b7433c0.us-east-1.aws.found.io";
 
     @Test
     public void test_ElasticSearch() throws IOException {
         restClient = RestClient.builder(new HttpHost(hostUrl,  9200)).build();
-
         //index a document
         HttpEntity httpEntity = new NStringEntity(
                 "{\n" +
@@ -34,22 +33,16 @@ public class ElasticSearchIntegrationTest {
                         "    \"post_date\" : \"2009-11-15T14:12:12\",\n" +
                         "    \"message\" : \"trying out Elasticsearch\"\n" +
                         "}", ContentType.APPLICATION_JSON);
-
         Response indexResponse = restClient.performRequest(
                 "PUT",
                 "/twitter/tweet/1",
                 Collections.<String, String>emptyMap(),
                 httpEntity);
-
-
         Response searchResponse = restClient.performRequest(
                 "GET",
                 "/twitter/tweet/_search?q=user:niko");
-
         String jsonString = IOUtils.toString(searchResponse.getEntity().getContent(), "UTF-8");
-
         assertTrue(jsonString.contains("niko"));
-
         restClient.close();
     }
 
